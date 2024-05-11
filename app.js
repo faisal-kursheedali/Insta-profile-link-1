eeeb726e6028284f12eec65e8b1fa9ffcf513db6
FAISAL <106899988+ibbbrrru@users.noreply.github.com>
2024-05-11 21:26:58 +0530

    fix

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const USER_ID = localStorage.getItem("userID")
 localStorage.setItem("userID", USER_ID);
 const dateTime = new Date().toISOString();
 let serverURL = "https://insta-profile-link.vercel.app/api";
+// let serverURL = "http://localhost:3000/api";
 
 fetch(`${serverURL}/insta_load/${USER_ID}/${dateTime}`)
   .then((res) => res.json())
