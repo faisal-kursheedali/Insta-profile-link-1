f96c656f90684ad4d67db39cbadfb06110c5fd50
FAISAL <106899988+ibbbrrru@users.noreply.github.com>
2024-05-11 22:17:19 +0530

    Update ip.js

--- server/util/ip.js
+++ server/util/ip.js
@@ -1,12 +1,8 @@
 const requestIP = require("request-ip");
 
 const getIp = (req) => {
-  if (process.env.ENVIRONMENT === "DEVELOPMENT") {
-    return "49.37.195.80";
-  } else {
-    const ip = requestIP.getClientIp(req);
+  const ip = requestIP.getClientIp(req);
 
-    return ip;
-  }
+  return ip;
 };
 module.exports = getIp;
